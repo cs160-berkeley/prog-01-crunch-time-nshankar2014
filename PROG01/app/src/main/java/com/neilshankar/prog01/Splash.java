2eb9042cc33539dc4796f09fbb178381eb63d32f
Neil Shankar <neil.s.shankar@gmail.com>
2016-02-05 01:22:59 -0800

    5 sec splash, barebones

--- PROG01/app/src/main/java/com/neilshankar/prog01/Splash.java
+++ PROG01/app/src/main/java/com/neilshankar/prog01/Splash.java
@@ -2,12 +2,25 @@ package com.neilshankar.prog01;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.content.Intent;
 
 public class Splash extends AppCompatActivity {
+    /* code help from http://bit.ly/1PEsjPa */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Splash.this.startActivity(new Intent(Splash.this, ChooseExercise.class));
+                Splash.this.finish();
+            }
+        }, 5000);
+
     }
+
 }
