4a7a6d93543a9743adb9b51eb3b1d3db833610f3
Neil Shankar <neil.s.shankar@gmail.com>
2016-02-05 15:10:08 -0800

    basic click listener, better code org

--- PROG01/app/src/main/java/com/neilshankar/prog01/ChooseExercise.java
+++ PROG01/app/src/main/java/com/neilshankar/prog01/ChooseExercise.java
@@ -1,24 +1,44 @@
 package com.neilshankar.prog01;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.ImageButton;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.util.Log;
 import android.content.Intent;
+import android.util.Log;
 
 public class ChooseExercise extends AppCompatActivity {
+
+    private final static String TAG = "ChooseExercise";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_exercise);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
+        LinearLayout fullGrid = (LinearLayout)findViewById(R.id.fullGrid);
+
+        // attach a click listener to each ImageButton
+        for (int i = 0; i < fullGrid.getChildCount(); i += 1) {
+            View v = fullGrid.getChildAt(i);
+            if (v instanceof LinearLayout) {
+                LinearLayout c = (LinearLayout) v;
+                for (int j = 0; j < c.getChildCount(); j += 1) {
+                    ImageButton ib = (ImageButton) c.getChildAt(j);
+                    ib.setClickable(true);
+                    Log.d(TAG, "" + ib.getTag());
+                    ib.setOnClickListener(new View.OnClickListener() {
+                        public void onClick(View clicked) {
+                            ChooseExercise.this.startActivity(new Intent(ChooseExercise.this, Pushups.class));
+                        }
+                    });
+                }
+            }
+        }
+
     }
 
     @Override
@@ -45,5 +65,14 @@ public class ChooseExercise extends AppCompatActivity {
 
     public void gotoPushups(View v) {
         ChooseExercise.this.startActivity(new Intent(this, Pushups.class));
+    }
+    public void gotoSitups(View v) {
+        ChooseExercise.this.startActivity(new Intent(this, Situps.class));
+    }
+    public void gotoSquats(View v) {
+        ChooseExercise.this.startActivity(new Intent(this, Squats.class));
+    }
+    public void gotoLegLifts(View v) {
+        ChooseExercise.this.startActivity(new Intent(this, LegLifts.class));
     }
 }
