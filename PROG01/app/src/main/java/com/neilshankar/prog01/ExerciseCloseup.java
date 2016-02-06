3c53bf4a7fa07db48a4f55ff29970542f53055ba
Neil Shankar <neil.s.shankar@gmail.com>
2016-02-05 17:07:50 -0800

    exerciseCloseup in good shape. just needs button handler

--- PROG01/app/src/main/java/com/neilshankar/prog01/ExerciseCloseup.java
+++ PROG01/app/src/main/java/com/neilshankar/prog01/ExerciseCloseup.java
@@ -14,8 +14,30 @@ public class ExerciseCloseup extends AppCompatActivity {
 
         Intent it = getIntent();
         String type = it.getStringExtra("type");
-
         TextView title = (TextView)findViewById(R.id.closeupTitle);
-        title.setText("How many " + type + " did you do?");
+        TextView minutes = (TextView)findViewById(R.id.minutes);
+        minutes.setText("minutes");
+
+        if (type.equals("push-ups") || type.equals("sit-ups") || type.equals("squats")
+                || type.equals("jumping jacks") || type.equals("pull-ups")) {
+            title.setText("How many " + type + " did you do?");
+            minutes.setText("");
+        } else if (type.equals("leg lifts")) {
+            title.setText("For how long did you do leg lifts?");
+        } else if (type.equals("planks")) {
+            title.setText("For how long did you plank?");
+        } else if (type.equals("cycling")) {
+            title.setText("For how long did you cycle?");
+        } else if (type.equals("walking")) {
+            title.setText("For how long did you walk?");
+        } else if (type.equals("jogging")) {
+            title.setText("For how long did you jog?");
+        } else if (type.equals("swimming")) {
+            title.setText("For how long did you swim?");
+        } else if (type.equals("stair climbing")) {
+            title.setText("For how long did you climb stairs?");
+        }
+
+        
     }
 }
